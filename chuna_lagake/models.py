cfd7c21fbb3e69db7711ef283cac7bda1764ec37
= <shubhamchitnis123@gmail.com>
2020-05-15 16:19:19 +0530

    models updated

--- chuna_lagake/models.py
+++ chuna_lagake/models.py
@@ -1,33 +1,41 @@
 from chuna_lagake import db
+from datetime import datetime
 
 class User(db.Model):
-	__tablename__ = "user"
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(100), nullable=False)
 	username = db.Column(db.String(20), nullable=False, unique=True)
 	password = db.Column(db.String(256), nullable=False)
 	email = db.Column(db.String(100), nullable=False, unique=True)
 	feedbacks = db.relationship('Feedback', backref='user',lazy=True)
 	entries = db.relationship('Entry', backref='user',lazy=True)
 
+	def __repr__ (self):
+		return f"User('{self.username}','{self.password}','{self.email}')"
+
 class Feedback(db.Model):
-	__tablename__ = "feedback"
 	id = db.Column(db.Integer, primary_key=True)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 	feedback = db.Column(db.Text, nullable=False)
 
+	def __repr__ (self):
+		return f"Feedback('{User.query.get(self.user_id).username}','{self.feedback}')"
+
 class Menu(db.Model):
-	__tablename__ = 'menu'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(100), nullable=False, unique=True)
 	image = db.Column(db.String, nullable=False)
 	description = db.Column(db.Text, nullable=False)
 	entries = db.relationship('Entry', backref='product', lazy=True)
 
+	def __repr__ (self):
+		return f"Item('{self.name}','{self.description}','{self.image}')"
+
 class Entry(db.Model):
-	__tablename__ = 'entry'
 	id = db.Column(db.Integer, primary_key=True)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
 	product_id = db.Column(db.Integer, db.ForeignKey('menu.id'), nullable=False)
 	status = db.Column(db.Boolean)
-	timestamp = db.Column(db.String, nullable=False)
+	timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+	def __repr__ (self):
+		return f"Entry('{self.user.username}','{self.product.name}','{self.status}','{self.timestamp}')"	
