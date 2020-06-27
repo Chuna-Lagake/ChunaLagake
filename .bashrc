1768d211f0a90215c414cd91ab1194e631cac6e1
LoneRanger3008 <shubhamchitnis123@gmail.com>
2020-06-28 01:53:58 +0530

    commit

--- .bashrc
+++
@@ -1,3 +0,0 @@
-export MAIL_USER="chunalagake.official@gmail.com"
-export MAIL_PASS="paanwithshaan"
-export PROD_DB="postgres://kpmrusbememplz:75e5885c2e4712e816632cb9367ed5cc3adbf60e0ff7ea1fee0770f0fffff622@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d4iv58stu8uomp"
--- chuna_lagake/__init__.py
+++ chuna_lagake/__init__.py
@@ -8,11 +8,8 @@ from flask_mail import Mail
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c0e86bb97cf45f8f36c696128d94e3bd'
 
-ENV = 'prod'
-if ENV == 'prod':
-	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('PROD_DB')
-else:
-	print('uri error')
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://kpmrusbememplz:75e5885c2e4712e816632cb9367ed5cc3adbf60e0ff7ea1fee0770f0fffff622@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d4iv58stu8uomp"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
@@ -21,8 +18,8 @@ login_manager = LoginManager(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASS')
+app.config['MAIL_USERNAME'] = "chunalagake.official@gmail.com"
+app.config['MAIL_PASSWORD'] = "paanwithshaan"
 mail = Mail(app)
 
 from chuna_lagake import routes
