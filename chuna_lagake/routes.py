65ac65bf85654f2415fa59f535504f4c6bd6be4b
LoneRanger3008 <shubhamchitnis123@gmail.com>
2020-07-04 00:50:30 +0530

    recommender

--- chuna_lagake/routes.py
+++ chuna_lagake/routes.py
@@ -41,11 +41,10 @@ def products():
 	trending_items = [str(x+1) for x in num_bought[:5]]
 	
 	if current_user.is_authenticated:
-		if not current_user.ratings:
-			list_of_recommendations = convert_to_user_recommend(model, interactions, labels, item_features, [current_user.id])
-		else :
+		if len(current_user.ratings) == 0 :
 			list_of_recommendations = trending_items
-		return render_template('products.html', trending_items = trending_items, recommended_items = [str(x) for x in list_of_recommendations])
+		else :
+			list_of_recommendations = convert_to_user_recommend(model, interactions, labels, item_features, [current_user.id])		return render_template('products.html', trending_items = trending_items, recommended_items = [str(x) for x in list_of_recommendations])
 	
 	return render_template('products.html', trending_items = trending_items)
 
