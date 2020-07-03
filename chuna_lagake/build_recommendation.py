d48aacf374d89e62a50f9c2b135a24e38705b082
Maheswaran <mahestpm@gmail.com>
2020-07-03 15:47:51 +0530

    Recommednation Complete

---
+++ chuna_lagake/build_recommendation.py
@@ -0,0 +1,124 @@
+from chuna_lagake.models import Menu, User, Ratings
+from lightfm import LightFM
+from lightfm.data import Dataset
+import numpy as np
+import os, csv, json
+from itertools import islice
+
+menu, users, items, ratings, user_recommend = [], [], [], [], []
+
+paan_features = ['Banaras','Calcutta','Maghai','Sada','Meetha','Chocolate','Dry Fruit','Mango','Strawberry','Pineapple','Gold',
+ 'Kaju','Jelly','Rose','Shahi','Kesar','Vanilla','Masala','Khatta','Orange','White','Silver','RaatRani','Nutella','Special']
+
+for i in range(Menu.query.count()):
+    menu_item = Menu.query.filter_by(id='{}'.format(i+1)).first()
+    menu.append([menu_item.id, menu_item.name, menu_item.features])
+
+def get_data():
+    # user data
+    for i in range(User.query.count()):
+        user_dict = {}
+        user_data = User.query.filter_by(id='{}'.format(i+1)).first()
+        user_dict.update({"User_ID":user_data.id, "username":user_data.username})
+        users.append(user_dict)
+
+    # item data
+    for item in menu:
+        item_dict = {}
+        item_dict.update({"Item_ID":item[0], "Item_Name":item[1]})
+        for i in range(25):
+            item_dict.update({paan_features[i]:item[2][i]})
+        items.append(item_dict)
+
+    # Rating Data
+    user_flag = 0
+    for i in range(Ratings.query.count()):
+        rating_dict = {}
+        rating = Ratings.query.filter_by(id='{}'.format(i+1)).first()
+        rating_dict.update({"User_ID":rating.user_id, "Item_ID":rating.item_id, "Rating":rating.rating})
+        if rating.user_id not in user_recommend:
+            user_recommend.append(rating.user_id)
+            rating_dict.update({"Recommend_ID":user_flag})
+            user_flag += 1
+        else:
+            rating_dict.update({"Recommend_ID":user_recommend.index(rating.user_id)})
+        ratings.append(rating_dict)
+
+    return users, items, ratings
+
+def get_user_features():
+    return get_data()[0]
+
+def get_item_features():
+    return get_data()[1]
+
+def get_ratings():
+    return  get_data()[2]
+
+user_features, item_features_names, ratings = get_data()
+
+def add_item_features(dataset, feature):
+    dataset.fit_partial(items=(x['Item_ID'] for x in get_item_features()),
+                    item_features=(x[feature] for x in get_item_features()))
+
+
+def add_item_features(dataset, feature):
+    dataset.fit_partial(items=(x['Item_ID'] for x in get_item_features()),
+                    item_features=(x[feature] for x in get_item_features()))
+
+
+
+def train_model():
+    dataset = Dataset()
+    dataset.fit((x['User_ID'] for x in get_ratings()),
+                (x['Item_ID'] for x in get_ratings()))
+    for i in range(25):
+        add_item_features(dataset, paan_features[i])
+    (interactions, weights) = dataset.build_interactions(((x['User_ID'], x['Item_ID'])
+                                                          for x in get_ratings()))
+
+    item_features = dataset.build_item_features(((x['Item_ID'], [x['Banaras'], x['Calcutta'], x['Maghai'], x['Sada'],
+                                                x['Meetha'], x['Chocolate'], x['Dry Fruit'], x['Mango'], x['Strawberry'],
+                                                x['Pineapple'], x['Kaju'], x['Jelly'], x['Rose'], x['Shahi'], x['Kesar'],
+                                                x['Vanilla'], x['Masala'], x['Khatta'], x['Orange'], x['White'], x['Silver'],
+                                                x['RaatRani'], x['Nutella'], x['Special'], x['Gold']])
+                                                  for x in get_item_features()))
+
+    model = LightFM(loss='bpr')
+    model.fit(interactions, item_features=item_features)
+
+    labels = np.array([x['Item_ID'] for x in get_ratings()])
+    print("Model Trained Successfully.....")
+    return model, interactions, labels, item_features
+
+def sample_recommendation(model, data, labels, item_features, user_ids):
+    n_users, n_items = data.shape
+    for user_id in user_ids:
+        known_positives = labels[data.tocsr()[user_id].indices]
+
+        scores = model.predict(user_id, np.arange(n_items), item_features)
+
+        top_items = labels[np.argsort(-scores)]
+
+        print("USER ID: {}".format(user_recommend[user_id]))
+        print("   Known Positives:")
+
+        for x in known_positives:
+            print("      {}".format(item_features_names[x-1].get("Item_Name")))
+#             print("    {}".format(x))
+
+        print("   Recommmended:")
+
+        for x in top_items[:3]:
+            print("      {}".format(item_features_names[x-1].get("Item_Name")))
+
+
+def convert_to_user_recommend(model, interactions, labels, item_features, user_ids):
+    new_user_id = []
+    for user_id in user_ids:
+        new_user_id.append(user_recommend.index(user_id))
+    sample_recommendation(model, interactions, labels, item_features, new_user_id)
+
+def start(user_ids, model, interactions, labels, item_features):
+    # model, interactions, labels, item_features = train_model()
+    convert_to_user_recommend(model, interactions, labels, item_features, user_ids)
--- chunalagake_recommendation.py
+++ chunalagake_recommendation.py
@@ -1,3 +1,26 @@
-from chuna_lagake.recommendation import *
+from chuna_lagake.build_recommendation import *
 
-start([1, 2, 3])
+model, interactions, labels, item_features = train_model()
+
+print("_________________STARTING RECOMMENDATION ENGINE_________________\n\n\n")
+print("Enter:\n1 for just training\n2 for recommending on trained model\n3 for training and recommending\n")
+
+run = True
+while run:
+    input_val = int(input())
+    if input_val == 1:
+        model, interactions, labels, item_features = train_model()
+        print("________________________________________________________________")
+
+    elif input_val == 2:
+        start([1, 2], model, interactions, labels, item_features)
+        print("________________________________________________________________")
+
+    elif input_val == 3:
+        model, interactions, labels, item_features = train_model()
+        start([1, 2], model, interactions, labels, item_features)
+        print("________________________________________________________________")
+
+    elif input_val == 4:
+        print("Quitting Recommmendation Engine....")
+        run = False
